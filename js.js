22ed877d39e9e859703df434bdfdb87e34667d30
id39 <100359834+idme520@users.noreply.github.com>
2022-09-14 00:22:01 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+function kuang() {
+  let kuang = document.getElementById("biankuang");
+  let = document.getElementById("");
+  if (kuang.onclick.style.display === "") {
+    box.style.display = "";
+  } else {
+    //没用的,js还是有点不懂
+    box.display.style = "";
+  }
+}
+function caidanlan() {
+  //菜单
+  let as = document.getElementById("cai3");
+  if (as.style.display === "none") {
+    as.style.display = "block";
+  } else {
+    as.style.display = "none";
+  }
+}
+function shezhi() {
+  //设置
+  let as = document.getElementById("shezhi1");
+  if (as.style.display === "none") {
+    as.style.display = "block";
+  } else {
+    as.style.display = "none";
+  }
+}
